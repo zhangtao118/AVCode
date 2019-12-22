74977e2edbd139e0a630f3b4676e00308ef0829b
tb_zhangtao <1659806196@qq.com>
2019-12-23 00:48:23 +0800

    1

---
+++ app/src/main/java/com/anzt/avcode/MainActivity.java
@@ -0,0 +1,15 @@
+package com.anzt.avcode;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.res.AssetManager;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/anzt/avcode/views/ImageSurfaceView.java
@@ -0,0 +1,83 @@
+package com.anzt.avcode.views;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.PixelFormat;
+import android.graphics.PorterDuff;
+import android.graphics.Rect;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.SurfaceView;
+import android.view.SurfaceHolder;
+
+import com.anzt.avcode.R;
+
+public class ImageSurfaceView extends SurfaceView implements SurfaceHolder.Callback {
+
+    private SurfaceHolder surfaceHolder;
+    private Paint paint;
+    private Rect rect;
+    private Bitmap bitmap;
+
+    public ImageSurfaceView(Context context) {
+        super(context);
+        init();
+    }
+
+    public ImageSurfaceView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public ImageSurfaceView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                drawImage();
+            }
+        }).start();
+    }
+
+    private void init(){
+        this.setZOrderOnTop(true);
+        //得到控制器
+        surfaceHolder=getHolder();
+        //对surfaceview进行操作
+        surfaceHolder.addCallback(this);
+        surfaceHolder.setFormat(PixelFormat.TRANSLUCENT);
+        //创建画笔
+        paint=new Paint();
+        //创建图片对象
+        bitmap= BitmapFactory.decodeResource(getResources(), R.drawable.test);
+    }
+
+    private void drawImage() {
+        //锁定画布
+        Canvas canvas = surfaceHolder.lockCanvas();
+        canvas.drawColor(Color.TRANSPARENT, PorterDuff.Mode.CLEAR);
+        canvas.drawBitmap(bitmap,new Matrix(),paint);
+        //解锁画布
+        surfaceHolder.unlockCanvasAndPost(canvas);
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+
+    }
+}
